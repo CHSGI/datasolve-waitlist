568e4e36ab106ab4ec58ce3b03e90c610d3f0f09
Akamo Dolapo <dolapoakamo01@gmail.com>
2023-02-04 23:58:28 +0900

    first commit

---
+++ src/config/footer.js
@@ -0,0 +1,43 @@
+export const footer = {
+  sectionOne: {
+    logo: 'Hello',
+    subtitle:
+      'No matter how big or how small your IT problem – or whether you’re international or local – Data Solve will take on the challenge.',
+    copyRights: 'Copyright © 2022 Datasolve',
+  },
+  sectionTwo: {
+    title: 'Company',
+    company: [
+      {
+        name: 'Home',
+        link: '/',
+      },
+      {
+        name: 'Careers',
+        tag: 'Hiring!',
+        link: '/',
+      },
+      {
+        name: 'Services',
+        link: '/',
+      },
+    ],
+  },
+  sectionThree: {
+    title: 'Resources',
+    resources: [
+      {
+        name: 'Blog',
+        link: '/',
+      },
+      {
+        name: 'Products',
+        link: '/',
+      },
+      {
+        name: 'Services',
+        link: '/',
+      },
+    ],
+  },
+}
